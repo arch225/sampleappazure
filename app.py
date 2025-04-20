0faca5d27e5e1c05d8613bf978c428b5ba77963d
arch225 <rachit.promo@gmail.com>
2025-04-20 15:03:14 +0530

    check commit 3

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Kutte Ke Poot yaha mat moot"}
+    return {"message": "Sab bhagte phire aur main le ra maje",
+            "author": "Neeraj Pepsu Dil me base"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
